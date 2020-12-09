7c5c5bd1b1277a3f4e8f389b4a972e5187b7b800
Connor Smiley <connor.smiley@uoit.net>
2020-12-08 23:30:13 -0500

    switched_to_snake_case full_c full_sendgit push!

--- Steganography/main.cpp
+++ Steganography/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <array>
+#include <cstdio>
+#include <cstdint>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
@@ -7,57 +7,75 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 
-using byte = uint8_t;
-//using pixel = std::array<byte, 4>;
-struct channels {
-	byte a;
-	byte b;
-	byte g;
-	byte r;
-};
+using channel= uint8_t;
+using pixel = uint32_t;
 
-struct pixel {
-	channels channels;
-};
+inline void pack_r(pixel& rgba, channel r) {
+	rgba |= static_cast<pixel>(r);
+}
 
-//Maps a 2D index to a 1D index
+inline void pack_g(pixel& rgba, channel g) {
+	rgba |= static_cast<pixel>(g) << 8;
+}
+
+inline void pack_b(pixel& rgba, channel b) {
+	rgba |= static_cast<pixel>(b) << 16;
+}
+
+inline void pack_a(pixel& rgba, channel a) {
+	rgba |= static_cast<pixel>(a) << 24;
+}
+
+inline pixel pack_pixel(channel r, channel g, channel b, channel a) {
+	pixel rgba = 0;
+	pack_r(rgba, r);
+	pack_g(rgba, g);
+	pack_b(rgba, b);
+	pack_a(rgba, a);
+	return rgba;
+}
+
+inline void unpack_pixel(pixel rgba, channel& r, channel& g, channel& b, channel& a) {
+	a = (rgba & 0xFF000000) >> 24;
+	b = (rgba & 0x00FF0000) >> 16;
+	g = (rgba & 0x0000FF00) >> 8;
+	r = (rgba & 0x000000FF);
+}
+
 int index(int x, int y, int width) {
 	return y * width + x;
 }
 
-pixel readPixel(int x, int y, int width, byte* memory) {
-	pixel pixel;
-	//uint32_t channels = memory[index(x, y, width)];
-	pixel.channels.a = (memory[index(x + 0, y, width)] & 0xff000000) >> 24;
-	pixel.channels.b = (memory[index(x + 1, y, width)] & 0x00ff0000) >> 16;
-	pixel.channels.g = (memory[index(x + 2, y, width)] & 0x0000ff00) >> 8;
-	pixel.channels.r = (memory[index(x + 3, y, width)] & 0x000000ff);
-	return pixel;
+inline pixel read_pixel(int x, int y, int width, uint32_t* memory) {
+	return memory[index(x, y, width)];
 }
 
-uint32_t readPixel(int index, byte* memory) {
-	uint32_t value = 0;
-	memcpy(&value, memory + index, sizeof(uint32_t));
-	return value;
+inline void write_pixel(pixel value, int x, int y, int width, uint32_t* memory) {
+	memory[index(x, y, width)] = value;
 }
 
-void printPixel(byte r, byte g, byte b, byte a) {
-	printf("r: %u g: %u b: %u a: %u\n", r, g, b, a);
-}
-
-void printPixel(uint32_t rgba) {
+inline void print_pixel(pixel rgba) {
 	printf("%X\n", rgba);
 }
 
+inline void print_channel(channel c) {
+	printf("%u\n", c);
+}
+
 int main() {
-	const int32_t rgba = 4;
-	int32_t width = 0, height = 0;
+	const int components = 4;
+	int width = 0, height = 0;
+	uint32_t* memory = reinterpret_cast<uint32_t*>(stbi_load("res/test_input.png", &width, &height, nullptr, components));
+	const int row_bytes = width * components;
+
+	pixel before = 0xFCFDFEFF;
+	channel r, g, b, a;
+	unpack_pixel(before, r, g, b, a);
+	pixel after = pack_pixel(r, g, b, a);
 
 	//Channels are a, b, g, r (left to right, most to least significant bit) in memory!
-	byte* memory = stbi_load("res/test_input.png", &width, &height, nullptr, rgba);
-	const int32_t row_size = width * rgba;
-
 	//Memory of the first 5 pixels for the first two rows:
+
 	//0        //1      //2      //3      //4
 	//red      //green  //blue   //white  //black
 	//ff0000ff ff00ff00 ffff0000 ffffffff ff000000
@@ -66,80 +84,56 @@ int main() {
 	//black    //white  //blue   //green  //red
 	//ff000000 ffffffff ffff0000 ff00ff00 ff0000ff
 
-	byte b0 = memory[0];
-	byte b1 = memory[1];
-	byte b2 = memory[2];
-	byte b3 = memory[3];
-	uint32_t ba = readPixel(0, memory);
+	pixel p00 = read_pixel(0, 0, width, memory);
+	printf("P00 (red)\n");
+	print_pixel(p00);
 
-	byte b4 = memory[4];
-	byte b5 = memory[5];
-	byte b6 = memory[6];
-	byte b7 = memory[7];
-	uint32_t bb = readPixel(4, memory);
-	
-	byte b8 = memory[8];
-	byte b9 = memory[9];
-	byte b10 = memory[10];
-	byte b11 = memory[11];
-	uint32_t bc = readPixel(8, memory);
+	pixel p10 = read_pixel(1, 0, width, memory);
+	printf("\nP10 (green)\n");
+	print_pixel(p10);
 
-	printf("Red\n");
-	printPixel(b0, b1, b2, b3);
-	printPixel(ba);
+	pixel p20 = read_pixel(2, 0, width, memory);
+	printf("\nP20 (blue)\n");
+	print_pixel(p20);
 
-	printf("\nGreen\n");
-	printPixel(b4, b5, b6, b7);
-	printPixel(bb);
+	pixel p30 = read_pixel(3, 0, width, memory);
+	printf("\nP30 (white)\n");
+	print_pixel(p30);
 
-	printf("\nBlue\n");
-	printPixel(b8, b9, b10, b11);
-	printPixel(bc);
+	pixel p40 = read_pixel(4, 0, width, memory);
+	printf("\nP40 (black)\n");
+	print_pixel(p40);
 
-	//Layout is ABGR most to least significant.
-	printf("Break!\n");
+	printf("\nSecond row:\n");
+	pixel p01 = read_pixel(0, 1, width, memory);
+	printf("P01 (black)\n");
+	print_pixel(p01);
 
-	//1st row:
-	//pixel p00 = readPixel(0, 0, width, memory);	//Should be red.
-	//pixel p10 = readPixel(1, 0, width, memory);
-	//pixel p20 = readPixel(2, 0, width, memory);
-	//pixel p30 = readPixel(3, 0, width, memory);
-	//pixel p40 = readPixel(4, 0, width, memory);
-	//
-	////2nd row:
-	//pixel p01 = readPixel(0, 1, width, memory);	//Should be black.
-	//pixel p11 = readPixel(1, 1, width, memory);
-	//pixel p21 = readPixel(2, 1, width, memory);
-	//pixel p31 = readPixel(3, 1, width, memory);
-	//pixel p41 = readPixel(4, 1, width, memory);
+	pixel p11 = read_pixel(1, 1, width, memory);
+	printf("\nP11 (white)\n");
+	print_pixel(p11);
 
-	//Makes the first 2.5 rows white:
-	//for (int i = 0; i < 5; i++) {
-	//	byte* row = memory + i * row_size;
-	//	memset(row, 255, row_size);
-	//}
+	pixel p21 = read_pixel(2, 1, width, memory);
+	printf("\nP21 (blue)\n");
+	print_pixel(p21);
 
-	//Makes the first 2.5 rows white:
-	//for (int i = 0; i < 5; i++) {
-	//	byte* row = memory + i * row_size;
-	//	memset(row, 255, row_size / 2);
-	//}
+	pixel p31 = read_pixel(3, 1, width, memory);
+	printf("\nP31 (green)\n");
+	print_pixel(p31);
+
+	pixel p41 = read_pixel(4, 1, width, memory);
+	printf("\nP41 (red)\n");
+	print_pixel(p41);
 	
 	//Make the blue channel of each pixel fully blue:
-	//for (int y = 0; y < height; y++) {
-	//	for (int x = 0; x < width; x++) {
-	//		int i = index(x, y, width);
-	//		byte a = memory[i];
-	//		byte b = memory[i + 1];
-	//		byte g = memory[i + 2];
-	//		byte r = memory[i + 3];
-	//		printf("R: %i G: %i B: %i A: %i\n", r, g, b, a);
-	//	}
-	//}
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			const int i = index(x, y, width);
+		}
+	}
 
-	//Works
-	//stbi_write_png("res/test_output.png", width, height, rgba, memory, width * rgba);
-
+	stbi_write_png("res/test_output.png", width, height, components, memory, row_bytes);
 	stbi_image_free(memory);
-	return EXIT_SUCCESS;
+	
+	return getchar();
 }
