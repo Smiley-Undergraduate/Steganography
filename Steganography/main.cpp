3eaab3ba9e2179b7bd97800454ccb8b94115809a
Connor Smiley <connor.smiley@uoit.net>
2020-12-08 22:05:59 -0500

    Starting to make sense of memory layout

--- Steganography/main.cpp
+++ Steganography/main.cpp
@@ -35,6 +35,20 @@ pixel readPixel(int x, int y, int width, byte* memory) {
 	return pixel;
 }
 
+uint32_t readPixel(int index, byte* memory) {
+	uint32_t value = 0;
+	memcpy(&value, memory + index, sizeof(uint32_t));
+	return value;
+}
+
+void printPixel(byte r, byte g, byte b, byte a) {
+	printf("r: %u g: %u b: %u a: %u\n", r, g, b, a);
+}
+
+void printPixel(uint32_t rgba) {
+	printf("%X\n", rgba);
+}
+
 int main() {
 	const int32_t rgba = 4;
 	int32_t width = 0, height = 0;
@@ -45,23 +59,59 @@ int main() {
 
 	//Memory of the first 5 pixels for the first two rows:
 	//0        //1      //2      //3      //4
+	//red      //green  //blue   //white  //black
 	//ff0000ff ff00ff00 ffff0000 ffffffff ff000000
+
 	//5        //6      //7      //8      //9
+	//black    //white  //blue   //green  //red
 	//ff000000 ffffffff ffff0000 ff00ff00 ff0000ff
 
-	//1st row:
-	pixel p00 = readPixel(0, 0, width, memory);	//Should be red.
-	pixel p10;
-	pixel p20;
-	pixel p30;
-	pixel p40;
+	byte b0 = memory[0];
+	byte b1 = memory[1];
+	byte b2 = memory[2];
+	byte b3 = memory[3];
+	uint32_t ba = readPixel(0, memory);
 
-	//2nd row:
-	pixel p01 = readPixel(0, 1, width, memory);	//Should be black.
-	pixel p11;
-	pixel p21;
-	pixel p31;
-	pixel p41;
+	byte b4 = memory[4];
+	byte b5 = memory[5];
+	byte b6 = memory[6];
+	byte b7 = memory[7];
+	uint32_t bb = readPixel(4, memory);
+	
+	byte b8 = memory[8];
+	byte b9 = memory[9];
+	byte b10 = memory[10];
+	byte b11 = memory[11];
+	uint32_t bc = readPixel(8, memory);
+
+	printf("Red\n");
+	printPixel(b0, b1, b2, b3);
+	printPixel(ba);
+
+	printf("\nGreen\n");
+	printPixel(b4, b5, b6, b7);
+	printPixel(bb);
+
+	printf("\nBlue\n");
+	printPixel(b8, b9, b10, b11);
+	printPixel(bc);
+
+	//Layout is ABGR most to least significant.
+	printf("Break!\n");
+
+	//1st row:
+	//pixel p00 = readPixel(0, 0, width, memory);	//Should be red.
+	//pixel p10 = readPixel(1, 0, width, memory);
+	//pixel p20 = readPixel(2, 0, width, memory);
+	//pixel p30 = readPixel(3, 0, width, memory);
+	//pixel p40 = readPixel(4, 0, width, memory);
+	//
+	////2nd row:
+	//pixel p01 = readPixel(0, 1, width, memory);	//Should be black.
+	//pixel p11 = readPixel(1, 1, width, memory);
+	//pixel p21 = readPixel(2, 1, width, memory);
+	//pixel p31 = readPixel(3, 1, width, memory);
+	//pixel p41 = readPixel(4, 1, width, memory);
 
 	//Makes the first 2.5 rows white:
 	//for (int i = 0; i < 5; i++) {
@@ -88,7 +138,7 @@ int main() {
 	//}
 
 	//Works
-	stbi_write_png("res/test_output.png", width, height, rgba, memory, width * rgba);
+	//stbi_write_png("res/test_output.png", width, height, rgba, memory, width * rgba);
 
 	stbi_image_free(memory);
 	return EXIT_SUCCESS;
