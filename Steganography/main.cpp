de3c706370a59443cc459168402a6638c7eedb79
Connor Smiley <connor.smiley@uoit.net>
2021-07-01 23:36:44 -0400

    Made re-usable tint function and verified endien-ness

--- Steganography/main.cpp
+++ Steganography/main.cpp
@@ -1,8 +1,11 @@
 #pragma region includes
 #include <cstdio>
 #include <cstdint>
+#include <cassert>
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <bitset>
 #include <string>
 
@@ -18,6 +21,14 @@ using byte = uint8_t;
 using channel= uint8_t;
 using pixel = uint32_t;
 #pragma endregion
+#pragma optimize("", off)
+
+enum class CHANNEL : int {
+	R,
+	G,
+	B,
+	A
+};
 
 #pragma region packing
 inline void pack_r(pixel& rgba, channel r) {
@@ -36,7 +47,27 @@ inline void pack_a(pixel& rgba, channel a) {
 	rgba |= static_cast<pixel>(a) << 24;
 }
 
-inline pixel pack_pixel(channel r, channel g, channel b, channel a) {
+inline void pack(pixel& rgba, channel value, CHANNEL type) {
+	switch (type)
+	{
+	case CHANNEL::R:
+		pack_r(rgba, value);
+		break;
+	case CHANNEL::G:
+		pack_g(rgba, value);
+		break;
+	case CHANNEL::B:
+		pack_b(rgba, value);
+		break;
+	case CHANNEL::A:
+		pack_a(rgba, value);
+		break;
+	default:
+		break;
+	}
+}
+
+inline pixel pack(channel r, channel g, channel b, channel a) {
 	pixel rgba = 0;
 	pack_r(rgba, r);
 	pack_g(rgba, g);
@@ -63,7 +94,27 @@ inline void unpack_a(pixel rgba, channel& a) {
 	a = (rgba & 0xFF000000) >> 24;
 }
 
-inline void unpack_pixel(pixel rgba, channel& r, channel& g, channel& b, channel& a) {
+inline void unpack(pixel rgba, channel& value, CHANNEL type) {
+	switch (type)
+	{
+	case CHANNEL::R:
+		unpack_r(rgba, value);
+		break;
+	case CHANNEL::G:
+		unpack_g(rgba, value);
+		break;
+	case CHANNEL::B:
+		unpack_b(rgba, value);
+		break;
+	case CHANNEL::A:
+		unpack_a(rgba, value);
+		break;
+	default:
+		break;
+	}
+}
+
+inline void unpack(pixel rgba, channel& r, channel& g, channel& b, channel& a) {
 	unpack_r(rgba, r);
 	unpack_g(rgba, g);
 	unpack_b(rgba, b);
@@ -71,16 +122,18 @@ inline void unpack_pixel(pixel rgba, channel& r, channel& g, channel& b, channel
 }
 #pragma endregion
 
-int index(int x, int y, int width) {
-	return y * width + x;
+int index(int x, int y, int width, int height) {
+	int result = y * width + x;
+	assert(result < width* height);
+	return result;
 }
 
-inline pixel read_pixel(int x, int y, int width, uint32_t* memory) {
-	return memory[index(x, y, width)];
+inline pixel read_pixel(int x, int y, int width, int height, uint32_t* memory) {
+	return memory[index(x, y, width, height)];
 }
 
-inline void write_pixel(pixel value, int x, int y, int width, uint32_t* memory) {
-	memory[index(x, y, width)] = value;
+inline void write_pixel(pixel value, int x, int y, int width, int height, uint32_t* memory) {
+	memory[index(x, y, width, height)] = value;
 }
 
 inline void print_pixel(pixel rgba) {
@@ -104,32 +157,6 @@ inline bool get_bit(byte b, int i) {
 	return b & (1 << i);
 }
 
-struct test {
-	char arr[3];
-};
-
-void serialize(const test& t, int xStart, int yStart, int arrayWidth, pixel* memory) {
-	const int bitCount = sizeof(t) * 8;
-	const int byteCount = sizeof(t);
-	const int pixelCount = byteCount / sizeof pixel + (byteCount % sizeof(pixel) == 0 ? 0 : 1);
-
-	const uint8_t* byteAddress = reinterpret_cast<const uint8_t*>(&t);
-	
-	//for all pixels
-	for (int i = 0; i < pixelCount; i++) {
-		//for all bytes in a pixel (4)
-		for (int i = 0; i < sizeof(pixel); i++) {
-			uint8_t byte = *byteAddress;
-			//for all bytes in a bit (8, highest-to-lowest)
-			for (int i = 7; i >= 0; i--) {
-				//Windows is little endian which is apparently least to most significant bit (right to left ascending rather than left to right).
-				//TLDR I need testing before I write this!
-			}
-			byteAddress++;
-		}
-	}
-}
-
 template<typename T>
 std::string to_binary(T t) {
 	std::bitset<sizeof(t) * 8> bits(t);
@@ -141,42 +168,95 @@ void print_binary(T t) {
 	std::cout << to_binary(t) << std::endl;
 }
 
+struct test {
+	long memes;
+	//char a = 1;
+	//short b = 2;
+	//int c = 3;
+	//long long d = 0xF1;
+};
+
+void serialize(const test& t, int xStart, int yStart, int arrayWidth, int arrayHeight, pixel* memory) {
+	//Data is not guaranteed to fit perfectly into pixels.
+	//We ensure we have enough pixels to store our data by analyzing the remainder of bytes divided by bytes per pixel.
+	const int bitCount = sizeof(t) * 8;
+	const int byteCount = bitCount / 8;
+	const int pixelCount = byteCount / sizeof pixel + byteCount % sizeof pixel == 0 ? 0 : 1;
+	const uint8_t* byteAddress = reinterpret_cast<const uint8_t*>(&t);
+}
+
+void test_serialize(std::ofstream& output, const test& input) {
+	const int byteCount = sizeof(input);
+	const int bitCount = byteCount * 8;
+	std::vector<bool> bits(bitCount);
+	//std::bitset<bitCount> bits;
+
+	byte* address = reinterpret_cast<byte*>(const_cast<test*>(&input));
+	for (int i = 0; i < byteCount; i++) {
+		for (int j = 7; j >= 0; j--) {
+			bool bit = get_bit(*address, j);
+			bits.push_back(bit);
+		}
+		address++;
+	}
+
+	for (bool i : bits) {
+		int representation = i;
+		output << representation;
+	}
+}
+
+void test_deserialize(std::ifstream& input, test& output) {
+
+}
+
+//Overwrites channel values if pointers are non-null.
+void write(const channel* r, const channel* g, const channel* b, const channel* a, int width, int height, pixel* memory) {
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			pixel rgba = read_pixel(x, y, width, height, memory);
+			if (r != nullptr) pack_r(rgba, *r);
+			if (g != nullptr) pack_g(rgba, *g);
+			if (b != nullptr) pack_b(rgba, *b);
+			if (a != nullptr) pack_a(rgba, *a);
+			write_pixel(rgba, x, y, width, height, memory);
+		}
+	}
+}
+
 int main() {
-	//Big endian means the most significant byte is at the lowest address (towards the end of the data).
-	//ie 0x0F01 is 0000 1111 0000 0001  
+	test t;
+	serialize(t, 0, 0, 0, 0, nullptr);
+	
+	//1. Serialize test
+	//std::ofstream output("test.txt");
+	//test_serialize(output, t);
+	//output.close();
 
-	//Little endian means the least significant byte is at the lowest address (towards the start of the data).
-	//ie 0x0F01 is 0000 0001 0000 1111
+	//2. Deserialize test
+	//std::ifstream input("test.txt");
 
+
+	//Big endian puts the most significant bit (largest value) first.
+	//Litte endian puts the least significant bit (smallest value) first.
+	uint16_t memes = 0x0301;
 	uint16_t u16 = 0;
-	byte& rl = *reinterpret_cast<byte*>(&u16);			//low (least significant)
-	byte& rh = *(reinterpret_cast<byte*>(&u16) + 1);	//high (most significant)
-	byte* p_rl = &rl;
-	byte* p_rh = &rh;
-
+	byte& rl = *reinterpret_cast<byte*>(&u16);
+	byte& rh = *(reinterpret_cast<byte*>(&u16) + 1);
 	rl = 0x01;
-	rh = 0x0F;
-
-	uint16_t* pu16 = &u16;
+	rh = 0x03;
 	print_binary(u16);
+	print_binary(memes);
 
-	//Windows is little endian.
-	//Our pixel memory layout is ABGR where A is most significant and R is least significant.
-	//If we set A to 255 and R to 1, our memory should look like:
-	//0000 0001 0000 0000 0000 0000 1111 1111 in little endian!
-
+	//Windows is big endian and most significant bits are being displayed first. Everything checks out!
 	pixel rgba = 0;
 	pack_a(rgba, 255);
 	pack_r(rgba, 1);
-	pixel* p_rgba = &rgba;
 	print_binary(rgba);
-
 	pixel hex = 0xFF000001;
 	print_binary(hex);
 
-	//The displays look like big endian. The most significant bits are printed first... No entiendo!
-
-	const int components = 4;
+	/*const int components = 4;
 	int width = 0, height = 0;
 
 	//First five pixels are red, green, blue, black white
@@ -186,45 +266,26 @@ int main() {
 	const int row_bytes = width * components;
 	const int col_bytes = height * components;
 	const int all_bytes = width * height * components;
+	const channel full = 0xFF;
 
 	//Make a copy of the original image memory so we can restore it after overwriting.
-	pixel* memory_copy = reinterpret_cast<pixel*>(malloc(all_bytes));
+	pixel* memory_copy = static_cast<pixel*>(malloc(all_bytes));
 	memcpy(memory_copy, memory, all_bytes);
 
-	//Red tint:
 	memcpy(memory, memory_copy, all_bytes);
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			pixel rgba = read_pixel(x, y, width, memory);
-			pack_r(rgba, 0xFF);
-			write_pixel(rgba, x, y, width, memory);
-		}
-	}
+	write(&full, nullptr, nullptr, nullptr, width, height, memory);
 	stbi_write_png("res/test_output_red.png", width, height, components, memory, row_bytes);
 
-	//Green tint:
 	memcpy(memory, memory_copy, all_bytes);
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			pixel rgba = read_pixel(x, y, width, memory);
-			pack_g(rgba, 0xFF);
-			write_pixel(rgba, x, y, width, memory);
-		}
-	}
+	write(nullptr, &full, nullptr, nullptr, width, height, memory);
 	stbi_write_png("res/test_output_green.png", width, height, components, memory, row_bytes);
 	
-	//Blue tint:
 	memcpy(memory, memory_copy, all_bytes);
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			pixel rgba = read_pixel(x, y, width, memory);
-			pack_b(rgba, 0xFF);
-			write_pixel(rgba, x, y, width, memory);
-		}
-	}
+	write(nullptr, nullptr, &full, nullptr, width, height, memory);
 	stbi_write_png("res/test_output_blue.png", width, height, components, memory, row_bytes);
 
 	stbi_image_free(memory);
-	free(memory_copy);
+	free(memory_copy);*/
+	printf("Done!\n");
 	return getchar();
 }
